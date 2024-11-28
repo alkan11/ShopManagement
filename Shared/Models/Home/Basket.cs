038adf16e9fe3d01ec4105bc74c9040bbc74206e
Alkan <simsekalkan11@gmail.com>
2024-11-28 22:57:58 +0300

    Basket modal Create and edit

---
+++ Shared/Models/Home/Basket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models.Home
+{
+	public class Basket
+	{
+        public decimal Price { get; set; }
+        public float Amount { get; set; }
+        public int ProductId { get; set; }
+    }
+	public class RepeaterFormModel
+	{
+		public List<Basket> Baskets { get; set; }
+	}
+}
--- YufkaDashboard.Web/Controllers/HomeController.cs
+++ YufkaDashboard.Web/Controllers/HomeController.cs
@@ -1,12 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Shared.Models.Home;
+using System.Reflection;
+using YufkaDashboard.Business.Abstract;
 
 namespace YufkaDashboard.Web.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly IConfiguration _configuration; 
+        private readonly ISystemBusiness _systemBusiness;
+        private readonly IProductBusiness _productBusiness;
+
+		public HomeController(ISystemBusiness systemBusiness, IConfiguration configuration, IProductBusiness productBusiness)
+		{
+			_systemBusiness = systemBusiness;
+			_configuration = configuration;
+			_productBusiness = productBusiness;
+		}
+
+		public async Task<IActionResult> Index()
         {
+            var allProducts = await _productBusiness.GetAllProducts();
+            ViewBag.AllProducts = allProducts.Data;
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddBasket(RepeaterFormModel basket)
+        {
+			foreach (var product in basket.Baskets)
+			{
+				// Her ürün bilgisine erişim
+				Console.WriteLine($"ProductId: {product.ProductId}, Quantity: {product.Amount}, Total: {product.Price}");
+			}
+			return View();
         }
         public IActionResult Index2()
         {
