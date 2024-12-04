107336e239cd7df55c02f8412f29ec4534316c83
alkan <alkan@volitek.com.tr>
2024-12-04 15:51:22 +0300

    doküman modülü geliştirmeleri

---
+++ YufkaDashboard.Web/Controllers/DocumentsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YufkaDashboard.Web.Controllers
+{
+	public class DocumentsController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
